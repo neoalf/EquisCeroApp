edd9c2080cda12cf5241b5cbb0edbc561e4ceb07
neoalf <ultraneom@gmail.com>
2025-09-19 23:02:49 -0600

    Ya con botones.

--- app/src/main/java/com/example/equisceroapp/Juego.kt
+++ app/src/main/java/com/example/equisceroapp/Juego.kt
@@ -1,12 +1,29 @@
 package com.example.equisceroapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Juego : AppCompatActivity() {
+
+    lateinit var b1: Button
+    lateinit var b2: Button
+    lateinit var b3: Button
+    lateinit var b4: Button
+    lateinit var b5: Button
+    lateinit var b6: Button
+    lateinit var b7: Button
+    lateinit var b8: Button
+    lateinit var b9: Button
+
+    var currentPlayer:Int = 1
+    var scorePlayer1:Int = 0
+    var scorePlayer2:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +32,38 @@ class Juego : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+        initUI()
+
     }
+
+
+private fun Juego.initUI() {
+    b1 = findViewById(R.id.b1)
+    b2 = findViewById(R.id.b2)
+    b3 = findViewById(R.id.b3)
+    b4 = findViewById(R.id.b4)
+    b5 = findViewById(R.id.b5)
+    b6 = findViewById(R.id.b6)
+    b7 = findViewById(R.id.b7)
+    b8 = findViewById(R.id.b8)
+    b9 = findViewById(R.id.b9)
+
+}
+fun play(btn:View){
+    val myBtn:Button = btn as Button
+if(myBtn.text.toString().isEmpty()){
+        if(currentPlayer == 1){
+            myBtn.text = "X"
+            currentPlayer = 2
+        }else{
+
+            myBtn.text = "O"
+            currentPlayer = 1
+
+            }
+    }
+}
 }
--- app/src/main/java/com/example/equisceroapp/MainActivity.kt
+++ app/src/main/java/com/example/equisceroapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.equisceroapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fun nextScreen(v:View){
+            var player1 = findViewById<EditText>(R.id.etplayer1)
+            var player2 = findViewById<EditText>(R.id.etplayer2)
+
+            var intent = Intent(applicationContext, Juego ::class.java)
+            intent.putExtra("player1", player1.text.toString())
+            intent.putExtra("player2", player2.text.toString())
+            startActivity(intent)
+
+        }
+
     }
 }
